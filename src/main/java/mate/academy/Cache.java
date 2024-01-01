3ca5af912bb0ea119024e9c45e8d0f87553ca871
Сидоркін Андрій Володимирович <sydorkin-av@ukrposhta.ua>
2024-01-01 22:26:36 +0200

    fairness fix

--- src/main/java/mate/academy/Cache.java
+++ src/main/java/mate/academy/Cache.java
@@ -2,61 +2,55 @@ package mate.academy;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReadWriteLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class Cache<K, V> {
     private final Map<K, V> cacheMap = new HashMap<>();
-    private final ReadWriteLock lock = new ReentrantReadWriteLock(true);
+    private final ReadWriteLock lock = new ReentrantReadWriteLock();
 
     public V get(K key) {
-        Lock readlock = lock.readLock();
-        readlock.lock();
+        lock.readLock().lock();
         try {
             return cacheMap.get(key);
         } finally {
-            readlock.unlock();
+            lock.readLock().unlock();
         }
     }
 
     public void put(K key, V value) {
-        Lock writelock = lock.writeLock();
-        writelock.lock();
+        lock.writeLock().lock();
         try {
             cacheMap.put(key, value);
         } finally {
-            writelock.unlock();
+            lock.writeLock().unlock();
         }
     }
 
     public void remove(K key) {
-        Lock writelock = lock.writeLock();
-        writelock.lock();
+        lock.writeLock().lock();
         try {
             cacheMap.remove(key);
         } finally {
-            writelock.unlock();
+            lock.writeLock().unlock();
         }
     }
 
     public void clear() {
-        Lock writelock = lock.writeLock();
-        writelock.lock();
+        lock.writeLock().lock();
         try {
             cacheMap.clear();
         } finally {
-            writelock.unlock();
+            lock.writeLock().unlock();
         }
     }
 
     public int size() {
-        Lock readlock = lock.readLock();
-        readlock.lock();
+        lock.readLock().lock();
         try {
             return cacheMap.size();
         } finally {
-            readlock.unlock();
+            lock.readLock().unlock();
         }
     }
 }
