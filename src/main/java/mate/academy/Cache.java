26853e6e5bff895b740f0171a33f7d0aa25f4a82
denysSmeiNewBom <den.smei.ds@gmail.com>
2024-01-01 19:47:19 +0200

    Task completed

--- src/main/java/mate/academy/Cache.java
+++ src/main/java/mate/academy/Cache.java
@@ -1,23 +1,66 @@
 package mate.academy;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
 public class Cache<K, V> {
+    private Map<K, V> cash = new HashMap<>();
+    private ReadWriteLock lock = new ReentrantReadWriteLock(true);
+
     public V get(K key) {
-        return null;
+        Lock readLock = lock.readLock();
+        readLock.lock();
+        V element;
+        try {
+            element = cash.get(key);
+        } finally {
+            readLock.unlock();
+        }
+        return element;
     }
 
     public void put(K key, V value) {
-
+        Lock writeLock = lock.writeLock();
+        writeLock.lock();
+        try {
+            cash.put(key, value);
+        } finally {
+            writeLock.unlock();
+        }
     }
 
     public void remove(K key) {
-
+        Lock writeLock = lock.writeLock();
+        writeLock.lock();
+        try {
+            cash.remove(key);
+        } finally {
+            writeLock.unlock();
+        }
     }
 
     public void clear() {
-
+        Lock writeLock = lock.writeLock();
+        writeLock.lock();
+        try {
+            cash.clear();
+        } finally {
+            writeLock.unlock();
+        }
     }
 
     public int size() {
-        return -1;
+        Lock readLock = lock.readLock();
+        readLock.lock();
+        int size;
+        try {
+            size = cash.size();
+        } finally {
+            readLock.unlock();
+        }
+        return size;
     }
 }
