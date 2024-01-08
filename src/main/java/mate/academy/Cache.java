8a011c87576506a3a2b8be2d4220a0f9baa78bee
WallRun <dimonty2003@gmail.com>
2024-01-08 13:50:46 +0200

    initial commit

--- src/main/java/mate/academy/Cache.java
+++ src/main/java/mate/academy/Cache.java
@@ -1,23 +1,37 @@
 package mate.academy;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
 public class Cache<K, V> {
+    private final Map<K, V> storage = new HashMap<>();
+    private final ReadWriteLock lock = new ReentrantReadWriteLock();
+
     public V get(K key) {
-        return null;
+        return storage.get(key);
     }
 
     public void put(K key, V value) {
-
+        lock.writeLock().lock();
+        storage.put(key, value);
+        lock.writeLock().unlock();
     }
 
     public void remove(K key) {
-
+        lock.writeLock().lock();
+        storage.remove(key);
+        lock.writeLock().unlock();
     }
 
     public void clear() {
-
+        lock.writeLock().lock();
+        storage.clear();
+        lock.writeLock().unlock();
     }
 
     public int size() {
-        return -1;
+        return storage.size();
     }
 }
