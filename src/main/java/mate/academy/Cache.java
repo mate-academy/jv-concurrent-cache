e3a92a3e50cc949978bc89a917eb18bc8396c2bf
Bohdan Markatov <bogdanmarkatov@gmail.com>
2024-01-29 15:21:58 +0200

    implement methods

--- src/main/java/mate/academy/Cache.java
+++ src/main/java/mate/academy/Cache.java
@@ -1,23 +1,59 @@
 package mate.academy;
 
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
 public class Cache<K, V> {
+    private ConcurrentHashMap<K, V> hashMap;
+    private AtomicInteger size;
+    private ReentrantReadWriteLock rw = new ReentrantReadWriteLock(true);
+
+    public Cache() {
+        this.size = new AtomicInteger(0);
+        this.hashMap = new ConcurrentHashMap<>();
+    }
+
     public V get(K key) {
-        return null;
+        rw.readLock().lock();
+        try {
+            return hashMap.get(key);
+        } finally {
+            rw.readLock().unlock();
+        }
     }
 
     public void put(K key, V value) {
-
+        rw.writeLock().lock();
+        try {
+            hashMap.put(key, value);
+            size.incrementAndGet();
+        } finally {
+            rw.writeLock().unlock();
+        }
     }
 
     public void remove(K key) {
-
+        rw.writeLock().lock();
+        try {
+            hashMap.remove(key);
+            size.decrementAndGet();
+        } finally {
+            rw.writeLock().unlock();
+        }
     }
 
     public void clear() {
-
+        rw.writeLock().lock();
+        try {
+            hashMap.clear();
+            size.set(0);
+        } finally {
+            rw.writeLock().unlock();
+        }
     }
 
     public int size() {
-        return -1;
+        return size.get();
     }
 }
