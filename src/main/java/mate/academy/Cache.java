4f0f22c38ca973391218e5282120b0be66cb0d2d
mIst <bogdankharchenko77@gmail.com>
2024-02-08 23:25:48 +0200

    jv-concurrent-cache solution added

--- src/main/java/mate/academy/Cache.java
+++ src/main/java/mate/academy/Cache.java
@@ -1,23 +1,55 @@
 package mate.academy;
 
+import java.util.HashMap;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
 public class Cache<K, V> {
+    private final HashMap<K, V> storage = new HashMap<>();
+    private final ReadWriteLock readWriteLock = new ReentrantReadWriteLock();
+
     public V get(K key) {
-        return null;
+        readWriteLock.readLock().lock();
+        try {
+            return storage.get(key);
+        } finally {
+            readWriteLock.readLock().unlock();
+        }
     }
 
     public void put(K key, V value) {
-
+        readWriteLock.writeLock().lock();
+        try {
+            storage.put(key, value);
+        } finally {
+            readWriteLock.writeLock().unlock();
+        }
     }
 
     public void remove(K key) {
-
+        readWriteLock.writeLock().lock();
+        try {
+            storage.remove(key);
+        } finally {
+            readWriteLock.writeLock().unlock();
+        }
     }
 
     public void clear() {
-
+        readWriteLock.writeLock().lock();
+        try {
+            storage.clear();
+        } finally {
+            readWriteLock.writeLock().unlock();
+        }
     }
 
     public int size() {
-        return -1;
+        readWriteLock.readLock().lock();
+        try {
+            return storage.size();
+        } finally {
+            readWriteLock.readLock().unlock();
+        }
     }
 }
