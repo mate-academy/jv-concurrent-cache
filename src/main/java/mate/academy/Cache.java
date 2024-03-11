e19d9e5729b0ae0798ca93bf46d5d0602cf257d3
Roman Globa <globaroman@gmail.com>
2024-03-11 18:51:19 +0200

    Solving task

--- src/main/java/mate/academy/Cache.java
+++ src/main/java/mate/academy/Cache.java
@@ -1,23 +1,55 @@
 package mate.academy;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
 public class Cache<K, V> {
+    private final Map<K, V> map = new HashMap<>();
+    private final ReadWriteLock mutex = new ReentrantReadWriteLock();
     public V get(K key) {
-        return null;
+        mutex.readLock().lock();
+        try {
+            return map.get(key);
+        } finally {
+            mutex.readLock().unlock();
+        }
     }
 
     public void put(K key, V value) {
-
+        mutex.writeLock().lock();
+        try {
+            map.put(key, value);
+        } finally {
+            mutex.writeLock().unlock();
+        }
     }
 
     public void remove(K key) {
-
+        mutex.writeLock().lock();
+        try {
+            map.remove(key);
+        } finally {
+            mutex.writeLock().unlock();
+        }
     }
 
     public void clear() {
-
+        mutex.writeLock().lock();
+        try {
+            map.clear();
+        } finally {
+            mutex.writeLock().unlock();
+        }
     }
 
     public int size() {
-        return -1;
+        mutex.readLock().lock();
+        try {
+            return map.size();
+        } finally {
+            mutex.readLock().unlock();
+        }
     }
 }
