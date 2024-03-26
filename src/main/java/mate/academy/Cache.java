64e567e6dfc83b040184c8bbf26a5793fa7c0ddc
Artem Makovskyy <photomak79@gmail.com>
2024-03-26 20:26:04 +0200

    done

--- src/main/java/mate/academy/Cache.java
+++ src/main/java/mate/academy/Cache.java
@@ -1,23 +1,61 @@
 package mate.academy;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
 public class Cache<K, V> {
+    private ReadWriteLock lock;
+    private Map<K, V> cacheMap;
+
+    public Cache() {
+        this.cacheMap = new HashMap<>();
+        this.lock = new ReentrantReadWriteLock();
+    }
+
     public V get(K key) {
-        return null;
+        lock.readLock().lock();
+        try {
+            return cacheMap.get(key);
+        } finally {
+            lock.readLock().unlock();
+        }
     }
 
     public void put(K key, V value) {
-
+        lock.writeLock().lock();
+        try {
+            cacheMap.put(key, value);
+        } finally {
+            lock.writeLock().unlock();
+        }
     }
 
     public void remove(K key) {
-
+        lock.writeLock().lock();
+        try {
+            cacheMap.remove(key);
+        } finally {
+            lock.writeLock().unlock();
+        }
     }
 
     public void clear() {
-
+        lock.writeLock().lock();
+        try {
+            cacheMap.clear();
+        } finally {
+            lock.writeLock().unlock();
+        }
     }
 
     public int size() {
-        return -1;
+        lock.readLock().lock();
+        try {
+            return cacheMap.size();
+        } finally {
+            lock.readLock().unlock();
+        }
     }
 }
