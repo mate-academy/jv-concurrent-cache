641d9a5d31a520537435c012bc31e3e8ccf0a986
Solomia Bobyak <mia.bobyak@gmail.com>
2024-03-29 21:24:08 +0200

    implemented methods in the Cache class

--- src/main/java/mate/academy/Cache.java
+++ src/main/java/mate/academy/Cache.java
@@ -1,23 +1,57 @@
 package mate.academy;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
 public class Cache<K, V> {
+    private final Map<K, V> map = new HashMap<>();
+    private final ReadWriteLock readWriteLock = new ReentrantReadWriteLock();
+
     public V get(K key) {
-        return null;
+        Lock lock = readWriteLock.readLock();
+        lock.lock();
+        try {
+        return map.get(key);
+        } finally {
+            lock.unlock();
+        }
     }
 
     public void put(K key, V value) {
+        Lock lock = readWriteLock.writeLock();
+        lock.lock();
+        try {
+            map.put(key, value);
+        } finally {
+            lock.unlock();
+        }
 
     }
 
     public void remove(K key) {
-
+        Lock lock = readWriteLock.writeLock();
+        lock.lock();
+        try {
+            map.remove(key);
+        } finally {
+            lock.unlock();
+        }
     }
 
     public void clear() {
-
+        Lock lock = readWriteLock.writeLock();
+        lock.lock();
+        try {
+            map.clear();
+        } finally {
+            lock.unlock();
+        }
     }
 
     public int size() {
-        return -1;
+        return map.size();
     }
 }
