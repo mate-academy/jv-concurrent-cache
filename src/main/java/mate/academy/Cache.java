fd6c059186fd454f925e57bbb34f51063628af03
vbilodon <vovabilodon@gmail.com>
2024-04-21 15:36:27 +0300

    implemented Cache

--- src/main/java/mate/academy/Cache.java
+++ src/main/java/mate/academy/Cache.java
@@ -1,23 +1,57 @@
 package mate.academy;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
 public class Cache<K, V> {
+
+    private final ReadWriteLock lock = new ReentrantReadWriteLock();
+    private final Map<K, V> cache = new HashMap<>();
+
     public V get(K key) {
-        return null;
+        Lock readLock = lock.readLock();
+        readLock.lock();
+        try {
+            return cache.get(key);
+        } finally {
+            readLock.unlock();
+        }
     }
 
     public void put(K key, V value) {
-
+        Lock writeLock = lock.writeLock();
+        writeLock.lock();
+        try {
+            cache.put(key, value);
+        } finally {
+            writeLock.unlock();
+        }
     }
 
     public void remove(K key) {
-
+        Lock writeLock = lock.writeLock();
+        writeLock.lock();
+        try {
+            cache.remove(key);
+        } finally {
+            writeLock.unlock();
+        }
     }
 
     public void clear() {
-
+        Lock writeLock = lock.writeLock();
+        writeLock.lock();
+        try {
+            cache.clear();
+        } finally {
+            writeLock.unlock();
+        }
     }
 
     public int size() {
-        return -1;
+        return cache.size();
     }
 }
