108868050ad99802e468b26198c50302fcee0346
MolchanovAlexander <rybkolhoz@gmail.com>
2024-06-14 17:36:53 +0300

    build success

--- src/main/java/mate/academy/Cache.java
+++ src/main/java/mate/academy/Cache.java
@@ -1,23 +1,46 @@
 package mate.academy;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
 public class Cache<K, V> {
+    private final Map<K, V> innerCache = new HashMap<>();
+    private final ReadWriteLock readWriteLock = new ReentrantReadWriteLock();
+
     public V get(K key) {
-        return null;
+        return innerCache.get(key);
     }
 
     public void put(K key, V value) {
-
+        readWriteLock.writeLock().lock();
+        try {
+            innerCache.put(key, value);
+        } finally {
+            readWriteLock.writeLock().unlock();
+        }
     }
 
     public void remove(K key) {
-
+        readWriteLock.writeLock().lock();
+        try {
+            innerCache.remove(key);
+        } finally {
+            readWriteLock.writeLock().unlock();
+        }
     }
 
     public void clear() {
-
+        readWriteLock.writeLock().lock();
+        try {
+            innerCache.clear();
+        } finally {
+            readWriteLock.writeLock().unlock();
+        }
     }
 
     public int size() {
-        return -1;
+        return innerCache.size();
     }
 }
