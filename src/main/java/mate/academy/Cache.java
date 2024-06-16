952ba2a70ee62428e7c4dfdf9f57d3be4d8e0cf7
Mykola Skrypalov <skripalov1@gmail.com>
2024-06-16 20:12:01 +0300

    implemented "cache"

--- src/main/java/mate/academy/Cache.java
+++ src/main/java/mate/academy/Cache.java
@@ -1,23 +1,59 @@
 package mate.academy;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
 public class Cache<K, V> {
+    private final Map<K, V> cacheTable = new HashMap<>();
+    private final ReadWriteLock readWriteLock = new ReentrantReadWriteLock();
+    private final Lock readLock = readWriteLock.readLock();
+    private final Lock writeLock = readWriteLock.writeLock();
+
     public V get(K key) {
-        return null;
+        readLock.lock();
+        try {
+            return cacheTable.get(key);
+        } finally {
+            readLock.unlock();
+        }
     }
 
     public void put(K key, V value) {
-
+        writeLock.lock();
+        try {
+            cacheTable.put(key, value);
+        } finally {
+            writeLock.unlock();
+        }
     }
 
     public void remove(K key) {
-
+        writeLock.lock();
+        try {
+            cacheTable.remove(key);
+        } finally {
+            writeLock.unlock();
+        }
     }
 
     public void clear() {
-
+        writeLock.lock();
+        try {
+            cacheTable.clear();
+        } finally {
+            writeLock.unlock();
+        }
     }
 
     public int size() {
-        return -1;
+        readLock.lock();
+        try {
+            return cacheTable.size();
+        } finally {
+            readLock.unlock();
+        }
     }
 }
