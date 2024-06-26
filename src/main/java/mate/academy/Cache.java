692c687ec77bd61858aeaed6517a8ec12aea9100
Olesia Kapustynska <d.olleska@gmail.com>
2024-06-26 15:37:22 +0300

    add solution

--- src/main/java/mate/academy/Cache.java
+++ src/main/java/mate/academy/Cache.java
@@ -1,23 +1,57 @@
 package mate.academy;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
 public class Cache<K, V> {
+    private final Map<K, V> cacheMap = new HashMap<>();
+    private final ReadWriteLock lock = new ReentrantReadWriteLock();
     public V get(K key) {
-        return null;
+        lock.readLock();
+        try {
+            return cacheMap.get(key);
+        } finally {
+            lock.readLock().unlock();
+        }
     }
 
     public void put(K key, V value) {
-
+        lock.writeLock();
+        try {
+            cacheMap.put(key, value);
+        } finally {
+            lock.writeLock().unlock();
+        }
     }
 
     public void remove(K key) {
-
+        lock.writeLock();
+        try {
+            cacheMap.remove(key);
+        } finally {
+            lock.writeLock().unlock();
+        }
     }
 
     public void clear() {
-
+        lock.writeLock();
+        lock.readLock();
+        try {
+            cacheMap.clear();
+        } finally {
+            lock.writeLock().unlock();
+            lock.readLock().unlock();
+        }
     }
 
     public int size() {
-        return -1;
+        lock.readLock();
+        try {
+            return cacheMap.size();
+        } finally {
+            lock.readLock().unlock();
+        }
     }
 }
