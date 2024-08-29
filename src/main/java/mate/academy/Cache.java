062e2a244c6c07e16f6794ad05ad0f8b1b56284a
Piotr Kaliszuk <p_kaliszuk@tlen.pl>
2024-08-29 20:46:19 +0200

    solution

--- src/main/java/mate/academy/Cache.java
+++ src/main/java/mate/academy/Cache.java
@@ -1,23 +1,50 @@
 package mate.academy;
 
+import java.util.HashMap;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Cache<K, V> {
+    private final HashMap<K, V> map = new HashMap();
+    private final Lock lock = new ReentrantLock();
+
     public V get(K key) {
-        return null;
+        lock.lock();
+        try {
+            return map.get(key);
+        } finally {
+            lock.unlock();
+        }
     }
 
     public void put(K key, V value) {
-
+        lock.lock();
+        try {
+            map.put(key, value);
+        } finally {
+            lock.unlock();
+        }
     }
 
     public void remove(K key) {
-
+        lock.lock();
+        try {
+            map.remove(key);
+        } finally {
+            lock.unlock();
+        }
     }
 
     public void clear() {
-
+        lock.lock();
+        try {
+            map.clear();
+        } finally {
+            lock.unlock();
+        }
     }
 
     public int size() {
-        return -1;
+        return map.size();
     }
 }
