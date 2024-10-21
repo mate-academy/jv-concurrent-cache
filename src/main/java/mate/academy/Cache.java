8746c401fafa3ae261e9386185dff298748c0aec
Dmytro Busyhin <busyhin.d@gmail.com>
2024-10-22 00:00:17 +0300

    feat: implementation concurrent cache

--- src/main/java/mate/academy/Cache.java
+++ src/main/java/mate/academy/Cache.java
@@ -1,23 +1,56 @@
 package mate.academy;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
 public class Cache<K, V> {
+    private final Map<K, V> cacheMap = new HashMap<>();
+    private final ReadWriteLock lock = new ReentrantReadWriteLock();
+
     public V get(K key) {
-        return null;
+        try {
+            lock.readLock().lock();
+            return cacheMap.get(key);
+        } finally {
+            lock.readLock().unlock();
+        }
     }
 
     public void put(K key, V value) {
-
+        try {
+            lock.writeLock().lock();
+            cacheMap.put(key, value);
+        } finally {
+            lock.writeLock().unlock();
+        }
     }
 
     public void remove(K key) {
-
+        try {
+            lock.writeLock().lock();
+            cacheMap.remove(key);
+        } finally {
+            lock.writeLock().unlock();
+        }
     }
 
     public void clear() {
-
+        try {
+            lock.writeLock().lock();
+            cacheMap.clear();
+        } finally {
+            lock.writeLock().unlock();
+        }
     }
 
     public int size() {
-        return -1;
+        try {
+            lock.readLock().lock();
+            return cacheMap.size();
+        } finally {
+            lock.readLock().unlock();
+        }
     }
 }
