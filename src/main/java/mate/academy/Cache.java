6cda66c332eb2a89a082b0a8db0db755860708f7
Nataliya Plastun <natalya21753@gmail.com>
2025-01-02 20:05:28 +0200

    commit cache

--- src/main/java/mate/academy/Cache.java
+++ src/main/java/mate/academy/Cache.java
@@ -1,23 +1,61 @@
 package mate.academy;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
 public class Cache<K, V> {
+    private final Map<K, V> map;
+    private final ReadWriteLock lock;
+
+    public Cache() {
+        this.map = new HashMap<>();
+        this.lock = new ReentrantReadWriteLock();
+    }
+
     public V get(K key) {
-        return null;
+        lock.readLock().lock();
+        try {
+            return map.get(key);
+        } finally {
+            lock.readLock().unlock();
+        }
     }
 
     public void put(K key, V value) {
-
+        lock.writeLock().lock();
+        try {
+            map.put(key, value);
+        } finally {
+            lock.writeLock().unlock();
+        }
     }
 
     public void remove(K key) {
-
+        lock.writeLock().lock();
+        try {
+            map.remove(key);
+        } finally {
+            lock.writeLock().unlock();
+        }
     }
 
     public void clear() {
-
+        lock.writeLock().lock();
+        try {
+            map.clear();
+        } finally {
+            lock.writeLock().unlock();
+        }
     }
 
     public int size() {
-        return -1;
+        lock.readLock().lock();
+        try {
+            return map.size();
+        } finally {
+            lock.readLock().unlock();
+        }
     }
 }
